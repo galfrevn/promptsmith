c0434426ea569021df0428fce42cea4fabe96829
Franco Galfré <francogalfre.code@gmail.com>
2025-11-14 15:22:50 -0300

    refactor(metadata): 🔍 export metadata directly from lib

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -1,14 +1,10 @@
 import { geistMono, geistSans } from "@/styles/font";
 import "@/styles/globals.css";
 
-import type { Metadata } from "next";
+export { metadata } from "@/lib/metadata";
+
 import { Navbar } from "@/components/layout/navbar";
 import Footer from "./components/layout/footer";
-import { baseMetadata } from "./lib/metadata";
-
-export const metadata: Metadata = {
-  ...baseMetadata,
-};
 
 export default function RootLayout({
   children,
